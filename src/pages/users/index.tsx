5f16eecde076ad24cf5eada2038262d8843a139b
Alef Freitas <alef.freitas07@gmail.com>
2022-06-17 10:10:13 -0300

    chore: prefetch the data and use mutations

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Flex, Icon, Heading, Table, Thead, Tbody, Td, Tr, Th, Checkbox, Text, Spinner, useBreakpointValue } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Button, Flex, Icon, Heading, Link, Table, Thead, Tbody, Td, Tr, Th, Checkbox, Text, Spinner, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { useUsers } from '../../hooks/useUsers';
@@ -6,16 +7,29 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
-import Link from 'next/link';
+import NextLink from 'next/link';
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList(){
-
-  const { data, isLoading, isFetching, error } = useUsers();
+  
+  const [ page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string){
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`);
+
+      return response.data;
+    },{
+      staleTime: 1000 * 60 * 10, // 10 minutes
+    })
+  }
 
   return(
     <Box>
@@ -32,7 +46,7 @@ export default function UserList(){
               { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -42,7 +56,7 @@ export default function UserList(){
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           { isLoading ? (
@@ -66,7 +80,7 @@ export default function UserList(){
                 </Thead>
 
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return(
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -74,7 +88,9 @@ export default function UserList(){
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">{user.email}</Text>
                           </Box>
                         </Td>
@@ -98,9 +114,9 @@ export default function UserList(){
               </Table>
 
               <Pagination
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
 type User = {
@@ -9,6 +9,11 @@ type User = {
 
 export function makeServer(){
   const server = createServer({
+
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+
     models: {
       user: Model.extend<Partial<User>>({})
     },
@@ -57,7 +62,8 @@ export function makeServer(){
       });
 
 
-
+      
+      this.get('/users/:id');
       this.post('/users');
 
       this.namespace = '';
